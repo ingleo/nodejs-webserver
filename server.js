3a31c2322e8cfc75ff0a8d4d8ff60849527c2cfb
leonardo <clgrhc@gmail.com>
2019-06-11 03:14:17 -0500

    make it better

--- server.js
+++ server.js
@@ -3,25 +3,27 @@ const app = express();
 const hbs = require('hbs');
 require('./hbs/helpers');
 
+const port = process.env.PORT || 3000
+
 app.use(express.static(__dirname + '/public'));
 
 //express hbs engine
 hbs.registerPartials(__dirname + '/views/parciales');
 app.set('view engine', 'hbs');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('home', {
         nombre: 'leoNaRdo gONZaleZ',
         anio: new Date().getFullYear()
     });
 })
 
-app.get('/about', function(req, res) {
+app.get('/about', function (req, res) {
     res.render('about', {
         anio: new Date().getFullYear()
     });
 });
 
-app.listen(3000, () => {
-    console.log('Escuchando peticiones sobre el puerto 3000');
+app.listen(port, () => {
+    console.log(`Escuchando peticiones sobre el puerto ${port}`);
 });
